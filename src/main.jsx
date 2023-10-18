03fd94869eb5ca1a2ca0d7fd6347654a05dabe5d
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-10-19 03:45:55 +0600

    login & signin added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// react router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddProduct from './components/AddProduct.jsx';
+import SignUp from './components/SignUp.jsx';
+import Login from './components/Login.jsx';
+
+// router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  },
+  {
+    path: "/addProduct",
+    element: <AddProduct></AddProduct>
+  },
+  {
+    path: "/Login",
+    element: <Login></Login>
+  },
+  {
+    path: '/SignUp',
+    element: <SignUp></SignUp>
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
